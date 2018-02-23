7401f6a773cf8424982eed2d532b2da9691879e0
Uwe Klawitter <uweklawitter@cowabuja.de>
2018-02-23 16:07:03 +0100

    V0.0.1

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AccountDetailsPage} from "../account-details/account-details";
+import {QRScanner, QRScannerStatus} from "@ionic-native/qr-scanner";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(private navCtrl: NavController, private qrScanner: QRScanner) {
+    }
 
-  }
+    public openAccountDetails(): void {
+        this.navCtrl.push("AccountDetailsPage");
+    }
 
+    public scanQrCode(): void {
+        this.qrScanner.prepare()
+            .then((status: QRScannerStatus) => {
+                if (status.authorized) {
+                    let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+                        console.log("MARKER: text", text);
+                        this.qrScanner.hide();
+                        scanSub.unsubscribe();
+
+                        window.document.querySelector('ion-app').classList.remove('transparent-body')
+                    });
+
+                    this.qrScanner.show();
+                    window.document.querySelector('ion-app').classList.add('transparent-body');
+                } else if (status.denied) {
+                    //TODO
+                } else {
+                    //TODO
+                }
+            })
+            .catch((e: any) => console.log('Error is', e));
+    }
 }
