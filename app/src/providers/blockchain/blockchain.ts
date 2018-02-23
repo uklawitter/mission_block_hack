b85ca2b857557e52bc8be4d70c7482f96b5e6128
Philipp Seitz <philipp.seitz@kaufland.com>
2018-02-23 20:05:08 +0100

    hinein

--- app/src/providers/blockchain/blockchain.ts
+++ app/src/providers/blockchain/blockchain.ts
@@ -7,7 +7,8 @@ import {Account, Contract} from 'web3/types';
 // TODO: for debugging only
 const PRIVATE_KEY = "0xa0f2f1f1ecaca75f71f33b356e31821edaac19ec486fe3c097378e65bb22f63a";
 
-const KOVAN_TEST_NET = "https://kovan.infura.io";
+// const KOVAN_TEST_NET = "https://kovan.infura.io";
+const KOVAN_TEST_NET = "ws://localhost:8546";
 
 /*
   Generated class for the BlockchainProvider provider.
@@ -29,7 +30,7 @@ export class BlockchainProvider {
 
     constructor() {
         console.log('Initiate Blockchain Provider');
-        this.web3 = new Web3(new Web3.providers.HttpProvider(KOVAN_TEST_NET));
+        this.web3 = new Web3(new Web3.providers.WebsocketProvider(KOVAN_TEST_NET));
         this.account = this.web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
         this.addr.next(this.account.address);
         this.baseBonusCoinContract = new this.web3.eth.Contract([
@@ -222,14 +223,28 @@ export class BlockchainProvider {
                 "name": "TransactionFailed",
                 "type": "event"
             }
-        ]);
+        ], "" , { gasPrice: '20000000000'});
         console.log(this.web3);
         this.initAsync();
     }
 
     async initAsync() {
-        const map = this.bons.getValue().set("dummyaddr", {addr: "dummyaddr", logo: "assets/imgs/kl_logo.png", value: 7, name: "Koin", contract: null});
-        this.bons.next(map.set("dummyaddr2", {addr: "dummyaddr2", logo: "assets/imgs/logo_2.jpeg", value: 15, name: "Lidl Coin", contract: null}));
+        const contr: Contract = this.baseBonusCoinContract.clone();
+        contr.options.address = "0x841715D615fb5C053ed6104a59bB199def9B2852";
+        const map = this.bons.getValue().set("dummyaddr", {
+            addr: "0x841715D615fb5C053ed6104a59bB199def9B2852",
+            logo: "assets/imgs/kl_logo.png",
+            value: 7,
+            name: "Koin",
+            contract: null
+        });
+        this.bons.next(map.set("dummyaddr2", {
+            addr: "dummyaddr2",
+            logo: "assets/imgs/logo_2.jpeg",
+            value: 15,
+            name: "Lidl Coin",
+            contract: null
+        }));
         console.log(this.bons.getValue());
         const alive = await this.web3.eth.net.isListening();
         if (!alive) {
@@ -239,12 +254,23 @@ export class BlockchainProvider {
         console.log('web3 connected: ' + alive);
         console.log(this.account);
         console.log('balance: ' + await this.web3.eth.getBalance(this.account.address));
+
+
+        contr.events.BalanceChanged({filter: {wallet: this.account.address}}, (error, event) => {
+            console.log(event);
+            console.error(error);
+        });
+
+        console.log(await contr.methods.getBalance().call({from: this.account.address}));
+        // console.log(await contr.methods.withdraw('1234').send({from: this.account.address}));
+
     }
 
     async withDraw(scanInput: ScanInput): Promise<void> {
+        this.baseBonusCoinContract.clone()
     }
 
-    getSignedPublicKeyData() : {message: string} {
+    getSignedPublicKeyData(): { message: string } {
         const now = new Date();
         const tomorrow = new Date();
         tomorrow.setDate(now.getDate() + 1);
@@ -253,7 +279,7 @@ export class BlockchainProvider {
                 creation: now,
                 expires: tomorrow,
                 addr: this.account.address
-            }), this.account.privateKey) as {message: string};
+            }), this.account.privateKey) as { message: string };
     }
 
 }
