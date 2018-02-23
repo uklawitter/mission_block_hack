09a62584704ab308272a665592504e6fc5e88a04
Florian Barth <florianbarth@gmx.de>
2018-02-23 14:57:31 +0100

    Implement initial BonusCoinGenerator contract

--- contracts/BonusCoinGenerator.sol
+++ contracts/BonusCoinGenerator.sol
@@ -1,8 +1,19 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.19;
 
 import "./BonusCoinContract.sol";
 
 contract BonusCoinGenerator {
 
+  mapping (string => address) public deployedCoins;
+
+  function generateCoinContract(string coinName) public returns (address) {
+    if(deployedCoins[coinName] == address(0x0)){
+      BonusCoinContract a = new BonusCoinContract(coinName, msg.sender);
+      deployedCoins[coinName] =  a;
+      return a;
+    } else {
+      return deployedCoins[coinName];
+    }
+  }
 
 }
